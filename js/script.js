562833a868ff8075fafa8b002d5d8076bbd9e48d
Budin <apserbud22@gmail.com>
2022-04-09 17:17:56 +0300

    done 1

--- js/script.js
+++ js/script.js
@@ -4,14 +4,15 @@ let result ={};
 const first = +prompt("Укажите число");
 const exponent = +prompt("Укажите степень");
 
+math(first, exponent);
 function math(first, exponent=1) {
-    if(isNaN(first) || isNaN(exponent)) {
+    if(isNaN(first) || isNaN(exponent) || first === 0) {
         return alert('some error');
-    } else {
+    } else if(exponent === 0) {
+        exponent = 1;
         return alert(result = Math.pow(first, exponent));
-
+    }
+    else {
+        return alert(result = Math.pow(first, exponent));
     }
 }
-math(first, exponent)
-
-
